6626f0d0bb9d9bb8fe7d4489c4956a009370a36a
G6B0 <146021873+G6B0@users.noreply.github.com>
2024-06-09 21:08:33 -0400

    Inicio del Proyecto con Compresion LZ

---
+++ LZ77.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+struct code{
+	int o, l;
+	char c;
+};
+
+size_t memoria_usada(const vector<code>& v){
+	return v.size()* sizeof(code);
+}
+
+code find(string lab, string sb, int labs, int sbs){
+	code sol;
+	sol.o=0;
+	sol.l= 0;
+	sol.c= lab[0];
+	for(int j=sb.size()-1; j>=0; j--){
+		if(lab[0]== sb[j]){
+			int k=1, z= j+1, x= 0;
+			while(k<labs && z<sb.size() && lab[k]== sb[z]){
+				k++;
+				z++;
+			}
+			if(k<labs && z==sb.size()){
+				while(k<labs && lab[x]== lab[k]){
+					x++;
+					k++;
+				}
+			}
+			code pos;
+			pos.l= k;//prefix
+			pos.o= sb.size()- j;//offset
+			if(k==labs){
+				pos.c=' ';//blank
+			}else{
+				pos.c= lab[k];
+			}
+			if(pos.l> sol.l || (pos.l== sol.l && pos.o< sol.o)){
+				sol.l= pos.l;
+				sol.o= pos.o;
+				sol.c= pos.c;
+			}
+		}
+	}
+	return sol;
+}
+vector<code> encoding(string text, int labs, int sbs){
+	string lab, sb="";
+	int i=0; 
+	vector<code> v;
+	while(i<text.size()){
+		lab= text.substr(i, labs);
+		int sz= lab.size();
+		code sol= find(lab, sb, min(labs, sz), sbs);
+		v.push_back(sol);
+		int ptr= sol.l+1;
+		i+= ptr;
+		if(sb.size()==sbs){
+			if(ptr<sbs){
+				sb= sb.substr(ptr)+ lab.substr(0, ptr);
+			}else{
+				sb= lab.substr(0, ptr);
+			}
+		}else{
+			if(ptr+sb.size()<= sbs){
+				sb= sb+ lab.substr(0, ptr);
+			}else if(ptr<sbs){
+				sb= sb.substr(ptr+sb.size()- sbs)+ lab.substr(0, ptr);
+			}else{
+				sb= lab.substr(0, ptr);
+			}
+		}
+		if(sb.size()>sbs)
+			sb= sb.substr(sb.size()- sbs);
+	}
+	return v;
+}
+string decoding(vector<code> v){
+	string tex= "";
+	for(int i=0; i<v.size(); i++){
+		if(v[i].o==0){
+			tex+= v[i].c;
+		}else if(v[i].o<v[i].l){
+			string temp= "";
+			for(int j=0; j<v[i].l/v[i].o; j++){
+				temp+= tex.substr(tex.size()-v[i].o, v[i].o);
+			}
+			temp+= tex.substr(tex.size()-v[i].o, v[i].l%v[i].o)+ v[i].c;
+			tex+= temp;
+		}else{
+			tex= tex+ tex.substr(tex.size()-v[i].o, v[i].l)+ v[i].c;
+		}
+	}
+	return tex;
+}
+
+
+
+int main(){
+	int ws, labs, sbs;
+	cout<<"Ingrese el tamaño de la Ventana";
+	cin>>ws;
+	cout<<"Ingrese el tamaño del Buffer";
+	cin>>labs;
+	sbs= ws-labs;
+	cin.ignore();
+	string text;
+	cout<<"Enter text: ";
+	getline(cin,text);
+
+	return 0;
+
+}
